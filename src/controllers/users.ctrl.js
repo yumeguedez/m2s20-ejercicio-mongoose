19596aba7349c97003e0636b296381603d4ae02d
yumeguedez <yuguedez.dwfsc16@bootcamp.ucamp.io>
2023-07-20 11:44:17 -0500

    segundo commit

---
+++ src/controllers/users.ctrl.js
@@ -0,0 +1,31 @@
+const obtenerUsuarios = (req, res) => {
+  return res.json({
+    ok: true,
+    msg: "Usuarios obtenidos",
+    data: [],
+  });
+};
+
+const crearUsuario = (req, res) => {
+  return res.json({
+    ok: true,
+    msg: "Usuario creado",
+    data: {},
+  });
+};
+
+const actualizarUsuario = (req, res) => {
+  return res.json({
+    ok: true,
+    msg: "Usuario actualizado",
+    data: {},
+  });
+};
+
+const eliminarUsuario = (req, res) => {
+  return res.json({
+    ok: true,
+    msg: "Usuario eliminado",
+    data: {},
+  });
+};
