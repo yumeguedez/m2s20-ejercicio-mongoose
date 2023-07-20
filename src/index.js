840b7702b69c17676c90456af0abc6049551bd6b
yumeguedez <yuguedez.dwfsc16@bootcamp.ucamp.io>
2023-07-20 13:56:15 -0500

    quinto commit conexion establecida

--- src/index.js
+++ src/index.js
@@ -1,7 +1,9 @@
 require(`dotenv`).config();
 const express = require("express");
+const { dbConnection } = require("./database/config");
 const app = express();
 
+dbConnection();
 //leer cosas del body
 app.use(express.json());
 
